fb2aeeeac76727774fd7f3d5099576bb2dfb1992
compi231 <compi231@compi231.myguest.virtualbox.org>
2022-12-21 18:14:52 +0200

    added lambda opt in autobox

--- assignment2/Compi_HW2_tester-main/tp_sa.ml
+++ assignment2/Compi_HW2_tester-main/tp_sa.ml
@@ -1191,23 +1191,7 @@ module Semantic_Analysis : SEMANTIC_ANALYSIS = struct
          | _, _ -> ScmSeq'(new_sets @ [new_body]) in
        ScmLambda' (params, Simple, new_body)
     | ScmLambda' (params, Opt opt, expr') ->
-      let box_these =
-        List.filter
-          (fun param -> should_box_var param expr' (params@[opt]))
-          (params@[opt]) in
-      let new_body = 
-        List.fold_left
-          (fun body name -> box_sets_and_gets name body)
-          (auto_box expr')
-          box_these in
-      let new_sets = make_sets box_these (params@[opt]) in
-      let new_body = 
-        match box_these, new_body with
-        | [], _ -> new_body
-        | _, ScmSeq' exprs -> ScmSeq' (new_sets @ exprs)
-        | _, _ -> ScmSeq'(new_sets @ [new_body]) in
-      ScmLambda' (params, Opt opt, new_body)
-
+       raise X_not_yet_implemented
     | ScmApplic' (proc, args, app_kind) ->
        ScmApplic' (auto_box proc, List.map auto_box args, app_kind);;
 
