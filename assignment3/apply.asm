bf5d4f6db456fb723393f6c63a0a34ec641df45b
compi231 <compi231@compi231.myguest.virtualbox.org>
2023-01-19 18:08:47 +0200

    changed somethings in apply

--- assignment3/apply.asm
+++ assignment3/apply.asm
@@ -18,31 +18,35 @@ L_code_ptr_bin_apply:
 
         ;make values in the opposite order:
         ;1.pushing all argument one more time in the right order
-        mov rsi, rcx ;count-up
+        mov rbx, rcx ;count-up
         mov rcx, 0 
         mov rdx, rsp ;marking the begining of the second pushing
         my_loop2:
-                cmp rcx, rsi ; if rcx = n
+                cmp rcx, rbx ; if rcx = n
                 je my_loop_end2 ;then: jump to the end
                 mov rax, qword [rdx + 8 * rcx] ;else: rax = next arg in correct order
                 push rax
                 inc rcx
         my_loop_end2:
         ;2.overwriting element above by element below but in correct order
-        mov rcx, 1
+        lea rdx, [8 * (rbx + 6)] ;nubmer of bytes we need to skip
+        mov rsi, qword [rbp + 8 * 0] ; save old rbp
+        mov rdi, qword [rbp + 8 * 1] ; save return address
+        mov r8, qword [rbp + 8 * 4]  ; save function to apply
+        mov rcx, 0
         my_loop3:
-                cmp rcx, rsi ;if rcx = n
+                cmp rcx, rbx ;if rcx = n
                 je my_loop_end3 ;then: jump to the end
-                mov rax, qword [rsp + 8 * 1] ;else: get next arg in correct order
-                lea rbx, [rsp + 8 * rsi] ;address of arg that needs to be over written
-                mov [rbx], rax ;over writing arg in false order by arg with correct order
+                mov rax, qword [rsp + 8 * 0] ;else: get next arg in correct order
+                lea r9, [rsp + rdx] ;address of arg that needs to be over written
+                mov [r9], rax ;over writing arg in false order by arg with correct order
                 add rsp, 8 ;pop arg we used
                 inc rcx 
         my_loop_end3:
-        ;3.pushing number of arguments
-        push rcx
+        lea rsp, [rsp + 8 * rcx];pop all 1st time pushed args
+        add rsp, 8 * 3 ; pop old-rbp, return-address, le-ap
+        push rcx ;push number of arguments
+        
         mov rax, qword [rbp + 8 * 1] ;address of function to apply
         call rax ;apply the function
-        leave
-	ret
-
+        mov rsp, rbp; the part of leave we need
